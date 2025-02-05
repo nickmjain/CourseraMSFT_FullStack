534837ce278e3add62cb87a9dee1bbd7cf119cbb
nickm <nick.m.jain@gmail.com>
2025-02-04 19:46:20 -0500

    initial commit

---
+++ TestConsoleApp/ConsoleApp1/Module_3/countVowels.cs
@@ -0,0 +1,19 @@
+﻿
+public class countVowels
+{
+    public static void Run()
+    {
+        string text = "Hello World";
+        int vowelCount = 0;
+
+        foreach (char c in text.ToLower())
+        {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                vowelCount++;
+            }
+        }
+        Console.WriteLine("Hello World has 3 vowels");
+        Console.WriteLine("Vowel Count calculated by program: " + vowelCount);
+    }
+}
