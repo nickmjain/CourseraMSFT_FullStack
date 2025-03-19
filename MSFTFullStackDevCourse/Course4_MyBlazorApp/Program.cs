66d30cbfaa735f05c9a7770ae051524b23bad14f
nickm <nick.m.jain@gmail.com>
2025-03-19 16:44:13 -0400

    Done course 4, module 1

--- MSFTFullStackDevCourse/Course4_MyBlazorApp/Program.cs
+++ MSFTFullStackDevCourse/Course4_MyBlazorApp/Program.cs
@@ -1,15 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Course4_BlazorforFEDev.MyBlazorApp;
-using Course4_BlazorforFEDev.MyBlazorApp.services; // Ensure this matches the namespace of ApiService
+using Course4_MyBlazorApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// Register ApiService
-builder.Services.AddScoped<ApiService>();
+builder.Services.AddSingleton<ApiService>();
 
 await builder.Build().RunAsync();
+
--- MSFTFullStackDevCourse/Course4_MyBlazorApp/Services/ApiService.cs
+++ MSFTFullStackDevCourse/Course4_MyBlazorApp/Services/ApiService.cs
@@ -1,19 +1,24 @@
 using Microsoft.Extensions.Configuration;
 
 public class ApiService
+
 {
+
     private readonly IConfiguration _configuration;
 
-    // Construction: Inject IConfig to access appsettings
     public ApiService(IConfiguration configuration)
+
     {
+
         _configuration = configuration;
+
     }
 
-    // Method: ACcess the BaseUrl from appsettings.Development.json
-    public string GetApiURL()
+    public string GetApiUrl()
+
     {
-        return _configuration["ApiSettings:BaseUrl"];
-    }
 
+        return _configuration["ApiSettings:BaseUrl"];
+
+    }
 }
